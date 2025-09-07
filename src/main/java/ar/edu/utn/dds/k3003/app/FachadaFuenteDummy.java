e16fba131f506251e19b19dfd3907cb6f59a626c
Emiliano <emiliano.luzardo91@gmail.com.ar>
2025-09-06 22:16:00 -0300

    .

--- src/main/java/ar/edu/utn/dds/k3003/app/FachadaFuenteDummy.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/FachadaFuenteDummy.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@Profile("test")
 @Service
 public class FachadaFuenteDummy implements FachadaFuente {
     @Override
